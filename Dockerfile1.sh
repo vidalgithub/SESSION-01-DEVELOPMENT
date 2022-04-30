f0594a010f001904b98c9abd76ff3ce96cfe2e0b
Valdes Dockerfile1 <willy@valdes.com>
2022-04-30 18:18:16 +0300

    Creating Dockerfile1.sh from scratch

---
+++ Dockerfile1.sh
@@ -0,0 +1,9 @@
+FROM node:12-alpine
+RUN apk add --no-cache python2 g++ make
+WORKDIR /app
+COPY . .
+RUN yarn install --production
+CMD ["node", "src/index.js"]
+EXPOSE 3000
+
+########### Trying to see how it looks like #########
